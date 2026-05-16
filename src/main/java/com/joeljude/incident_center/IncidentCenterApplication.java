4e26ead55a66d8d0b10e6dddd5cf677ef66beb04
Joel Jude <joel@Joels-MacBook-Pro.local>
2026-05-16 16:01:12 -0400

    Initialize project structure

---
+++ src/main/java/com/joeljude/incident_center/IncidentCenterApplication.java
@@ -0,0 +1,13 @@
+package com.joeljude.incident_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joeljude/incident_center/IncidentCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joeljude.incident_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
